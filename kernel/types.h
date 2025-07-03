b24761caf67a1c09ed6ebc5c2751e8a95690b7f4
PietPadda <pietergdewaal@gmail.com>
2025-07-03 20:51:05 +0200

    feat: Setup fixed types header and IDT header

---
+++ kernel/types.h
@@ -0,0 +1,11 @@
+// myos/kernel/types.h
+#ifndef TYPES_H
+#define TYPES_H
+
+// Define fixed-width integer types for cross-platform compatibility
+typedef unsigned char   uint8_t;
+typedef unsigned short  uint16_t;
+typedef unsigned int    uint32_t;
+typedef unsigned long long uint64_t;
+
+#endif
