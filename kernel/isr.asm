c39fa3ea21ef72fc643d565a2930333b6fb2cb93
PietPadda <pietergdewaal@gmail.com>
2025-07-10 21:09:41 +0200

    feat: setup IRQ handlers and stubs

--- kernel/isr.asm
+++ kernel/isr.asm
@@ -53,6 +53,34 @@ ISR_NOERRCODE 29
 ISR_NOERRCODE 30
 ISR_NOERRCODE 31
 
+; --- Define the 16 IRQ handlers ---
+%macro IRQ 2
+global irq%1
+irq%1:
+    cli
+    push byte 0
+    push byte %2  ; Push the interrupt number
+    jmp irq_common_stub
+%endmacro
+
+; IRQs 0-15. Remember we remapped them to 32-47.
+IRQ 0, 32   ; Timer
+IRQ 1, 33   ; Keyboard
+IRQ 2, 34   ; Cascade
+IRQ 3, 35   ; COM2
+IRQ 4, 36   ; COM1
+IRQ 5, 37   ; LPT2
+IRQ 6, 38   ; Floppy Disk
+IRQ 7, 39   ; LPT1
+IRQ 8, 40   ; CMOS real-time clock
+IRQ 9, 41   ; Free for peripherals
+IRQ 10, 42  ; Free for peripherals
+IRQ 11, 43  ; Free for peripherals
+IRQ 12, 44  ; PS2 Mouse
+IRQ 13, 45  ; FPU
+IRQ 14, 46  ; Primary ATA Hard Disk
+IRQ 15, 47  ; Secondary ATA Hard Disk
+
 ; --- Common Assembly Stub ---
 ; All ISRs jump here after pushing their number and a (optional) error code
 extern fault_handler ; This is our C-level handler
@@ -99,4 +127,41 @@ isr_common_stub:
 
     ; 8. Clean up error code & interrupt number, and return
     add esp, 8
+    iret
+
+; --- Common IRQ Stub ---
+extern irq_handler ; Our new C-level IRQ handler
+irq_common_stub:
+    ; Save general purpose registers
+    pusha
+
+    ; Save segment registers
+    mov eax, ds
+    push eax
+    mov eax, es
+    push eax
+    mov eax, fs
+    push eax
+    mov eax, gs
+    push eax
+
+    ; Push a pointer to the registers and call the C handler
+    push esp
+    call irq_handler
+    add esp, 4 ; Clean up the pushed pointer
+
+    ; Restore segment registers
+    pop eax
+    mov gs, eax
+    pop eax
+    mov fs, eax
+    pop eax
+    mov es, eax
+    pop eax
+    mov ds, eax
+
+    ; Restore general purpose registers
+    popa
+    add esp, 8 ; Clean up the error code and interrupt number
+    sti
     iret
