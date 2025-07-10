f82ef91f9127900676a37f374221530c4825c37f
PietPadda <pietergdewaal@gmail.com>
2025-07-10 21:19:23 +0200

    feat: C-Level IRQ Handling Infrastructure

---
+++ kernel/irq.h
@@ -0,0 +1,14 @@
+// myos/kernel/irq.h
+
+#ifndef IRQ_H
+#define IRQ_H
+
+#include "types.h"
+#include "exceptions.h" // For registers_t struct
+
+void irq_install_handler(int irq, void (*handler)(registers_t *r));
+void irq_uninstall_handler(int irq);
+void irq_install();
+void irq_handler(registers_t *r);
+
+#endif
