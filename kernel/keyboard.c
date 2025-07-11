845272dd4acc16366f0ddd78eb9242010458af70
PietPadda <pietergdewaal@gmail.com>
2025-07-11 17:32:18 +0200

    feat(keyboard): implement Shift key support

--- kernel/keyboard.c
+++ kernel/keyboard.c
@@ -5,13 +5,26 @@
 #include "io.h"
 #include "vga.h"
 
-// Scancode set 1 for a US QWERTY keyboard layout.
+// --- State and Character Maps ---
+static int shift_pressed = 0;
+
+// Scancode set 1 for a US QWERTY keyboard layout - Normal
 // A 0 means the key is not printable.
 unsigned char kbd_us[128] = {
     0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
     '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
     0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0,
     '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0,
+    ' ', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '-', 0, 0, 0, '+',
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+};
+
+// Scancode set 1 for a US QWERTY keyboard layout - Shifted
+unsigned char kbd_us_shift[128] = {
+    0,  27, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\b',
+    '\t', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n',
+    0, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~', 0,
+    '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 0, '*', 0,
     ' ', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '-', 0, 0, 0, '+',
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 };
@@ -23,13 +36,27 @@ static void keyboard_handler(registers_t *r) {
     // Read the scancode from the keyboard's data port.
     scancode = port_byte_in(0x60);
 
-    // If the top bit of the byte is set, it means a key was released.
-    // We only care about key presses, so we ignore releases for now.
+    // --- Handle Key Releases ---
     if (scancode & 0x80) {
-        // Key release
+        // Remove the 0x80 bit to get the original press scancode
+        scancode &= 0x7F; 
+        if (scancode == 0x2A || scancode == 0x36) { // L/R Shift released
+            shift_pressed = 0;
+        }
+        
+    // --- Handle Key Presses ---
     } else {
-        // Key press
-        print_char(kbd_us[scancode]);
+        if (scancode == 0x2A || scancode == 0x36) { // L/R Shift pressed
+            shift_pressed = 1;
+        } else {
+            // Select the correct character map based on shift state
+            char character = shift_pressed ? kbd_us_shift[scancode] : kbd_us[scancode];
+            
+            // Only print if the character is valid (not 0)
+            if (character) {
+                print_char(character);
+            }
+        }
     }
 }
 
