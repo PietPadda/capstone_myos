380a2798506e8dd025f3ac0cbb56957de16f78ac
PietPadda <pietergdewaal@gmail.com>
2025-07-12 16:31:20 +0200

    feat(Memory): Added memtest malloc command to shell

--- kernel/shell.c
+++ kernel/shell.c
@@ -5,6 +5,7 @@
 #include "string.h" // Needed for strcmp
 #include "timer.h" // for tick getter
 #include "io.h" // for port_byte_out
+#include "memory.h" // for dynamic heap memory
 
 #define PROMPT "PGOS> "
 #define MAX_CMD_LEN 256
@@ -52,7 +53,7 @@ void shell_handle_input(char c) {
 void process_command() {
     // help command
     if (strcmp(cmd_buffer, "help") == 0) {
-        print_string("Available commands:\n  help - Display this message\n  cls  - Clear the screen\n  uptime  - Shows OS running time\n  reboot  - Reset the OS\n");
+        print_string("Available commands:\n  help - Display this message\n  cls  - Clear the screen\n  uptime  - Shows OS running time\n  reboot  - Reset the OS\n  memtest  - Print 3 dynamic heap addresses\n");
 
     // cls command
     } else if (strcmp(cmd_buffer, "cls") == 0) {
@@ -69,6 +70,16 @@ void process_command() {
         print_string("Rebooting system...");
         // Send the reboot command to the keyboard controller
         port_byte_out(0x64, 0xFE);
+
+    // memtest command
+    } else if (strcmp(cmd_buffer, "memtest") == 0) {
+        // Allocate a few blocks and print their addresses
+        void* addr1 = malloc(1);
+        void* addr2 = malloc(1);
+        void* addr3 = malloc(1024);
+        print_string("Block 1: "); print_hex((uint32_t)addr1);
+        print_string("\nBlock 2: "); print_hex((uint32_t)addr2);
+        print_string("\nBlock 3: "); print_hex((uint32_t)addr3);
 
     // invalid command
     } else if (cmd_index > 0) { // Only show error for non-empty commands
