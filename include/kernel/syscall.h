4552c4787f755ce1f86b750d43996c51c9e0c2fc
PietPadda <pietergdewaal@gmail.com>
2025-07-19 11:18:04 +0200

    refactor(syscall): implement syscall dispatch table

--- include/kernel/syscall.h
+++ include/kernel/syscall.h
@@ -3,6 +3,12 @@
 #ifndef SYSCALL_H
 #define SYSCALL_H
 
+// Forward-declare registers_t to avoid circular dependencies
+struct registers;
+
+// Define a type for our system call handler functions
+typedef void (*syscall_t)(struct registers *r);
+
 void syscall_install();
 
 #endif
--- kernel/syscall.c
+++ kernel/syscall.c
@@ -2,18 +2,38 @@
 
 #include <kernel/syscall.h>
 #include <kernel/vga.h>
-#include <kernel/exceptions.h> // For registers_t
+#include <kernel/exceptions.h> // For the full registers_t definition
 
-void syscall_install() {
-    // In the future, we would set up the syscall table here.
-    // For now, it's just a placeholder.
+#define MAX_SYSCALLS 32
+
+// The system call dispatch table
+static syscall_t syscall_table[MAX_SYSCALLS];
+
+// Our first syscall: a simple test print
+static void sys_test_print(registers_t *r) {
+    print_string("Syscall 1 called!\n");
 }
 
-// The C-level handler for all system calls
+void syscall_install() {
+    // Install the test print syscall at index 1
+    syscall_table[1] = &sys_test_print;
+}
+
+// The main C-level handler for all system calls
 void syscall_handler(registers_t *r) {
-    // For now, we only have one system call: print a message
-    // We'll use EAX to identify the syscall number
-    if (r->eax == 1) {
-        print_string("Syscall 1 called!\n");
+    // Get the syscall number from the EAX register
+    uint32_t syscall_num = r->eax;
+
+    // Check if the number is valid
+    if (syscall_num < MAX_SYSCALLS && syscall_table[syscall_num] != 0) {
+        // If it is, look up the handler in our table
+        syscall_t handler = syscall_table[syscall_num];
+        // And call it
+        handler(r);
+    } else {
+        // Invalid syscall number, print an error
+        print_string("Invalid syscall number: ");
+        print_hex(syscall_num);
+        print_char('\n');
     }
 }
