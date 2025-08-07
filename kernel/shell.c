da7a68f9e08a1f7a1416a2f028d210ac56f9c631
PietPadda <pietergdewaal@gmail.com>
2025-08-07 21:48:30 +0200

    feat(driver): add debug commands for pc speaker

--- kernel/shell.c
+++ kernel/shell.c
@@ -202,8 +202,8 @@ void process_command() {
 
     // help command
     if (strcmp(argv[0], "help") == 0) {
-        print_string("Available commands:\n  help - Display this message\n  cls  - Clear the screen\n  uptime  - Shows OS running time\n  reboot  - Reset the OS\n  memtest  - Allocate, free then recycle memory\n  cat  - Reads .txt file contents (needs arg)\n  disktest  - Read LBA19 (root dir)\n  sleep  - Stops OS for X ticks\n  ls  - List files in root dir\n  dump  - Dump the first 128b of root dir buffer\n  run  - Run user mode program\n  ps  - Show process list\n  kill - Reap a zombie process by PID\n  beep - A4 note using PC Speaker\n\n");
-
+        print_string("Available commands:\n  help - Display this message\n  cls  - Clear the screen\n  uptime  - Shows OS running time\n  reboot  - Reset the OS\n  memtest  - Allocate, free then recycle memory\n  cat  - Reads .txt file contents (needs arg)\n  disktest  - Read LBA19 (root dir)\n  sleep  - Stops OS for X ticks\n  ls  - List files in root dir\n  dump  - Dump the first 128b of root dir buffer\n  run  - Run user mode program\n  ps  - Show process list\n  kill - Reap a zombie process by PID\n  beep - A4 note using PC Speaker\n\n  --- DEBUG COMMANDS ---\n  starttone - Starts a continuous tone\n  stoptone  - Stops the continuous tone\n\n");
+    
     // cls command
     } else if (strcmp(argv[0], "cls") == 0) {
         clear_screen();
@@ -430,6 +430,16 @@ void process_command() {
     } else if (strcmp(argv[0], "beep") == 0) {
         print_string("Beeping!");
         beep(440, 500); // Beep at 440Hz (Note A4) for 500ms
+
+    // start tone command
+    } else if (strcmp(argv[0], "starttone") == 0) {
+        print_string("Starting 440Hz tone...");
+        play_sound(440);
+
+    // stop tone command
+    } else if (strcmp(argv[0], "stoptone") == 0) {
+        print_string("Stopping tone...");
+        nosound();
 
     // invalid command
     } else  { // Only show error for non-empty commands
